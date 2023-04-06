a3bb6876e9a7db68c83d198a406a17029fa3ce42
Jorge Luis Emmanuel Espinoza Espinoza <35707910+yoshi70001@users.noreply.github.com>
2023-04-06 17:20:59 -0500

    configuracion inicial

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { writeFileSync, fsyncSync } = require('fs');
+const jsdocMark = require('jsdoc-to-markdown');
+// const showdown = require('showdown'),
+//     converter = new showdown.Converter();
+
+const app = express();
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    next();
+});
+app.use(express.json());
+// app.use(cors);
+const port = 9000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>En linea</h1>');
+});
+app.post('/markdown', (req, res) => {
+    try {
+        writeFileSync('lib/test.js', req.body.body || '');
+        jsdocMark.render({ files: 'lib/test.js' }).then((respo) => {
+            res.status(200).send(respo || '');
+        });
+    } catch (error) {
+        res.status(500).send('ERROR al generar');
+    }
+    // console.log(req.body.body);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
